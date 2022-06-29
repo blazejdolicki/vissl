399dd2bd44001e243524efe497e1a02c660afcd8
Blazej Dolicki <blazej.dol@gmail.com>
2022-06-29 13:31:13 +0200

    Customize the script converting VISSL models to torchvision

--- extra_scripts/convert_vissl_to_torchvision.py
+++ extra_scripts/convert_vissl_to_torchvision.py
@@ -29,7 +29,7 @@ logging.basicConfig(level=logging.INFO, format=FORMAT, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 
-def convert_and_save_model(args, replace_prefix):
+def convert_and_save_model(args, replace_prefix, include_head=True):
     assert g_pathmgr.exists(args.output_dir), "Output directory does NOT exist"
 
     # load the model
@@ -49,15 +49,21 @@ def convert_and_save_model(args, replace_prefix):
         model_trunk = model
     logger.info(f"Input model loaded. Number of params: {len(model_trunk.keys())}")
 
+    if include_head:
+        model_head = model["classy_state_dict"]["base_model"]["model"]["heads"]
+        model_head = replace_module_prefix(model_head, prefix="0.clf.0.", replace_with="fc.")
+        model_trunk.update(model_head)
+
     # convert the trunk
     converted_model = replace_module_prefix(model_trunk, "_feature_blocks.")
+
     logger.info(f"Converted model. Number of params: {len(converted_model.keys())}")
 
     # save the state
     if args.output_name.endswith(".torch"):
-        output_filename = f"converted_vissl_{args.output_name}"
+        output_filename = f"converted_{args.output_name}"
     else:
-        output_filename = f"converted_vissl_{args.output_name}.torch"
+        output_filename = f"converted_{args.output_name}.torch"
     output_model_filepath = f"{args.output_dir}/{output_filename}"
     logger.info(f"Saving model: {output_model_filepath}")
     torch.save(converted_model, output_model_filepath)
@@ -85,8 +91,17 @@ def main():
     parser.add_argument(
         "--output_name", type=str, default=None, required=True, help="output model name"
     )
+
+    parser.add_argument(
+        "--include_head", action="store_true",
+        help="If specified, includes head in the state dict. Useful when converting the model after finetuning "
+             "or training a linear classifier on top of the pretrained model."
+    )
+
     args = parser.parse_args()
-    convert_and_save_model(args, replace_prefix="_feature_blocks.")
+
+    print("include_head", args.include_head)
+    convert_and_save_model(args, replace_prefix="_feature_blocks.", include_head=args.include_head)
 
 
 if __name__ == "__main__":
